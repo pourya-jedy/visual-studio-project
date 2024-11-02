211e2a08ce1e5876836ebdd2fbb9ee71681e9e0a
pourya-jedy <pourya.2005.official@gmail.com>
2024-11-02 07:10:44 -0700

    Add files via upload

---
+++ prime number.cs
@@ -0,0 +1,15 @@
+﻿int n = 1;
+int m = 9;
+bool isPrime =true;
+
+for (int j = n; j < m; j++)
+{
+    for (int i = j - 1; i > 1; i--)
+    {
+        if (j % i == 0)
+            isPrime = false;
+    }
+    if (isPrime) Console.WriteLine(j );
+    //else Console.Writeline(j + " Is not Prime");
+    isPrime = true;
+}
